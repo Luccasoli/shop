b816beaeefe145b88a34d806dcd9d6ffa7e6de70
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-04-23 22:05:31 -0300

    starting with shop implementation!

---
+++ lib/views/products_overview_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/mockup.dart';
+import 'package:shop/models/product.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = MOCK_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Minha loja',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+            itemCount: loadedProducts.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (context, i) {
+              return Text(this.loadedProducts[i].title);
+            }),
+      ),
+    );
+  }
+}
