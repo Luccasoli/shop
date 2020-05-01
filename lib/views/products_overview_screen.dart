a6920be13bf2c2245b9815a0522423becc24c4ad
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-05-01 19:25:25 -0300

    refactoring ProductsOverviewScreen

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/widgets/product_item.dart';
-
-import '../providers/products.dart';
+import 'package:shop/widgets/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<Product> loadedProducts = Provider.of<Products>(context).items;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -17,19 +12,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: GridView.builder(
-            itemCount: loadedProducts.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-            ),
-            itemBuilder: (context, i) {
-              return ProductItem(
-                loadedProducts[i],
-              );
-            }),
+        child: ProductGrid(),
       ),
     );
   }
---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<Product> loadedProducts = Provider.of<Products>(context).items;
+
+    return GridView.builder(
+      itemCount: loadedProducts.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (context, i) {
+        return ProductItem(
+          loadedProducts[i],
+        );
+      },
+    );
+  }
+}
