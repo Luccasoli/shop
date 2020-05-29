ee2a7417c0a7cf7d84c565c49e65e195e4e82013
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-05-29 08:03:44 -0300

    adding dismissible item functionality in CartScreen

--- lib/widgets/cart_item_widget.dart
+++ lib/widgets/cart_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,27 +9,52 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        vertical: 4,
-        horizontal: 15,
+    final theme = Theme.of(context);
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId);
+      },
+      background: Container(
+        color: theme.errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        padding: EdgeInsets.only(
+          right: 20,
+        ),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        alignment: Alignment.centerRight,
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text(
-                  cartItem.price.toString(),
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          vertical: 4,
+          horizontal: 15,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text(
+                    cartItem.price.toString(),
+                  ),
                 ),
               ),
             ),
+            title: Text(cartItem.title),
+            subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+            trailing: Text('${cartItem.quantity.toString()}x'),
           ),
-          title: Text(cartItem.title),
-          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
-          trailing: Text('${cartItem.quantity.toString()}x'),
         ),
       ),
     );
