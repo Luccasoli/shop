09009dbfcf478331bffe32db48196945d311f793
luccasoli <oliveira.mesquita@aluno.uece.br>
2020-05-29 09:28:34 -0300

    improving orders screen

--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -2,26 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/providers/orders.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem order;
 
   OrderItemWidget(this.order);
 
   @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
-      child: ListTile(
-        title: Text('R\$ ${order.total}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(
-            order.date,
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(
+                widget.order.date,
+              ),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
           ),
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: () {},
-        ),
+          if (_expanded)
+            Container(
+              height: (widget.order.products.length * 25.0) + 10,
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              child: ListView(
+                children: widget.order.products.map((product) {
+                  return Row(
+                    children: <Widget>[
+                      Text(
+                        product.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${product.quantity}x R\$ ${product.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  );
+                }).toList(),
+              ),
+            )
+        ],
       ),
     );
   }
